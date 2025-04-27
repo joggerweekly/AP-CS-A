2a2302f9227fef9d4f181a9fa082bedaba4cab59
joggerweekly <78917625+joggerweekly@users.noreply.github.com>
2025-04-27 10:35:16 -0700

    Create Sort.java

---
+++ Sort.java
@@ -0,0 +1,3 @@
+            // using the pre-defined function arraycopy
+            System.arraycopy(sublist_lower, 0, sublist, 0, sublist_lower.length);
+            System.arraycopy(sublist_higher_new, 0, sublist, sublist_lower.length, sublist_higher_new.length);
